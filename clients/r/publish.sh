91ff96701d6cd37f1bdc912ad170a25b13310db8
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:29:40 -0500

    claude autosave 2026-07-12T20:29:40Z

--- clients/r/publish.sh
+++ clients/r/publish.sh
@@ -4,5 +4,6 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version DESCRIPTION '^Version:'
 R CMD check --as-cran .
 [ "$PUBLISH_MODE" = dry-run ] || { R CMD build .; }
