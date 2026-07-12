ee60450c4301f4d4c7b5ba93d1a95ca683f4a94f
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:29:06 -0500

    claude autosave 2026-07-12T20:29:06Z

--- clients/rust/publish.sh
+++ clients/rust/publish.sh
@@ -4,5 +4,6 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version Cargo.toml '^version'
 cargo package
 [ "$PUBLISH_MODE" = dry-run ] || { cargo publish; }
