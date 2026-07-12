bfb55a9b4b340c6d6e2a41544ba04b9147f8f34c
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:29:50 -0500

    claude autosave 2026-07-12T20:29:50Z

--- clients/zig/publish.sh
+++ clients/zig/publish.sh
@@ -5,6 +5,7 @@ ROOT="$(CDPATH= cd -- "$DIR/../.." && pwd)"
 . "$ROOT/scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version build.zig.zon '\.version'
 zig build
 if [ "$PUBLISH_MODE" = release ]; then
   publish_git_tag "$ROOT" zig
