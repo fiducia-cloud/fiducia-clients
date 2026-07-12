35c9c16e04ef44ca016c679897a3fa6c574332df
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:30:17 -0500

    claude autosave 2026-07-12T20:30:17Z

--- clients/powershell/publish.sh
+++ clients/powershell/publish.sh
@@ -4,5 +4,6 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version Fiducia.psd1 'ModuleVersion'
 pwsh -NoLogo -NoProfile -Command 'Test-ModuleManifest ./Fiducia.psd1 | Out-Null'
 [ "$PUBLISH_MODE" = dry-run ] || { publish_require POWERSHELL_GALLERY_API_KEY; pwsh -NoLogo -NoProfile -Command 'Publish-Module -Path . -Repository PSGallery -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY'; }
