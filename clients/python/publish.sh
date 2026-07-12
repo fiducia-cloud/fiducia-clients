c32ef4752a890693f34856c26c47f7de5fdd4c60
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:30:44 -0500

    claude autosave 2026-07-12T20:30:44Z

--- clients/python/publish.sh
+++ clients/python/publish.sh
@@ -4,5 +4,6 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version pyproject.toml '^version'
 tmp="$(mktemp -d "${TMPDIR:-/tmp}/fiducia-python.XXXXXX")"; ${PYTHON:-python3} -m build --outdir "$tmp"; twine check "$tmp"/*
-[ "$PUBLISH_MODE" = dry-run ] || { twine upload "$tmp"/*; }
+[ "$PUBLISH_MODE" = dry-run ] || { twine upload --non-interactive "$tmp"/*; }
