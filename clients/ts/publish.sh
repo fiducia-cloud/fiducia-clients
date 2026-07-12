a7ada880f4445256c239ee7ba50daa3fb3c4b6d9
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:29:02 -0500

    claude autosave 2026-07-12T20:29:02Z

--- clients/ts/publish.sh
+++ clients/ts/publish.sh
@@ -4,5 +4,6 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version package.json '"version"'
 npm pack --dry-run
 [ "$PUBLISH_MODE" = dry-run ] || { npm publish --access public; }
