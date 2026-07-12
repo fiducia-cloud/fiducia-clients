16909a342b255c6d080fdc2d02622006503c645f
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:30:21 -0500

    claude autosave 2026-07-12T20:30:21Z

--- clients/julia/publish.sh
+++ clients/julia/publish.sh
@@ -5,6 +5,7 @@ ROOT="$(CDPATH= cd -- "$DIR/../.." && pwd)"
 . "$ROOT/scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version Project.toml '^version'
 julia --startup-file=no -e 'using TOML; TOML.parsefile("Project.toml")'
 if [ "$PUBLISH_MODE" = release ]; then
   publish_git_tag "$ROOT" julia
