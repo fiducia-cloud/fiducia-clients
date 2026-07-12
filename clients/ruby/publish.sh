126f2cd7e80f92c5698fd1e4f6a71b422bff463b
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:31:16 -0500

    claude autosave 2026-07-12T20:31:16Z

--- clients/ruby/publish.sh
+++ clients/ruby/publish.sh
@@ -4,6 +4,7 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version fiducia-client.gemspec '\.version'
 tmp="$(mktemp -d "${TMPDIR:-/tmp}/fiducia-ruby.XXXXXX")"
 gem_file="$tmp/fiducia-client.gem"
 gem build fiducia-client.gemspec --output "$gem_file"
