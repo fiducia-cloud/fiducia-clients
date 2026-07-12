3f91ccc8692013192e30f89596ba2eafd5a50e27
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:29:31 -0500

    claude autosave 2026-07-12T20:29:31Z

--- clients/clojure/publish.sh
+++ clients/clojure/publish.sh
@@ -4,5 +4,6 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version build.clj 'def version'
 clojure -T:build jar
 [ "$PUBLISH_MODE" = dry-run ] || { clojure -T:build deploy; }
