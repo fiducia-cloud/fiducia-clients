1150455fe4d5d7fe1be2f47135a37365024c7f14
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:29:45 -0500

    claude autosave 2026-07-12T20:29:45Z

--- clients/crystal/publish.sh
+++ clients/crystal/publish.sh
@@ -5,6 +5,7 @@ ROOT="$(CDPATH= cd -- "$DIR/../.." && pwd)"
 . "$ROOT/scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version shard.yml '^version:'
 crystal build --no-codegen src/fiducia.cr
 if [ "$PUBLISH_MODE" = release ]; then
   publish_git_tag "$ROOT" crystal
