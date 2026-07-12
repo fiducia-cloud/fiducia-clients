99730a6cc9b5ad5f34714bae6ca98142c4c212be
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-12 15:29:59 -0500

    claude autosave 2026-07-12T20:29:59Z

--- clients/kotlin/publish.sh
+++ clients/kotlin/publish.sh
@@ -4,5 +4,6 @@ DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 . "$DIR/../../scripts/publish-common.sh"
 publish_parse_mode "$@"
 cd "$DIR"
+publish_check_version build.gradle.kts '^version'
 gradle publishToMavenLocal --no-daemon
 [ "$PUBLISH_MODE" = dry-run ] || { gradle publish --no-daemon; }
